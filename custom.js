898d2c7d18795875800ce707e7f43fb35ed75120
David McGarry <beebul@gmail.com>
2017-11-16 08:42:39 +1030

    small additions

--- custom.js
+++ custom.js
@@ -11,4 +11,26 @@ $(document).ready(function() {
         return oldText === 'Yes' ? 'Next' : oldText;
     });
 
+    if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+	}
+
 });
