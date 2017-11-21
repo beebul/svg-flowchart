fe4b00c981916c66ec61a3ecc91064f90d654505
David McGarry <beebul@gmail.com>
2017-11-21 13:17:21 +1030

    changes

--- custom.js
+++ custom.js
@@ -1,40 +1,39 @@
 $(document).ready(function() {
-    
-    //change 'Yes' label to 'Next' for processes
-    // $('.process-group tspan').text(function(i, oldText) {
-    //     return oldText === 'Yes' ? 'Next' : oldText;
-    // });
 
-
-    $('.fc-process').fadeIn("slow", function() {
-        $('.fc-process').addClass("faded");
+    //legend clicks
+    $('.background').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-rhombus, .fc-finish").not('.orange').toggleClass('faded');
+    });
+    $('.meeting').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-rhombus, .fc-finish").not('.pink').toggleClass('faded');
+    });
+    $('.meetingResults').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-rhombus, .fc-finish").not('.yellow').toggleClass('faded');
+    });
+    $('.decision').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-finish").not('.fc-rhombus').toggleClass('faded');
+    });
+    $('.outcomes').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-rhombus, .fc-finish").not('.green').toggleClass('faded');
+    });
+    $('.formalEnquiry').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-rhombus, .fc-finish").not('.lightblue').toggleClass('faded');
+    });
+    $('.finalStep').click(function(){
+        $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        $(".fc-process, .fc-rhombus").not('.fc-finish').toggleClass('faded');
     });
 
-    // $('rect.fc-process').click(function(){
-    //     var allRects = $("rect");
-    // });
-
-    // $('rect.orange').click(function(){
-    //     var allRects = $("rect");
-    // });
-
-    // $('rect.orange').click(function(){
-    //     $(this).toggleClass('checked').siblings().addClass('fade');
-    //     console.log('a rect was clicked');
-    // })
-
-
-    // $("#loader").fadeIn("slow", function() {
-    //     $(this).addClass("loader");
-    // });
-
-
-    // $("#loader").fadeOut("slow", function() {
-    //     $(this).removeClass("loader");
-    // });
+    
 
                 
-    //back to top link
+    //back to top
     if ($('#back-to-top').length) {
     var scrollTrigger = 100, // px
         backToTop = function () {
@@ -54,7 +53,7 @@ $(document).ready(function() {
         $('html,body').animate({
             scrollTop: 0
         }, 700);
-    });
+        });
 	}
 
 });
