fd9794a7a4a565604dc982bde3947a1ca86ec8f1
David McGarry <beebul@gmail.com>
2017-11-22 13:56:06 +1030

    Current progress

--- custom.js
+++ custom.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
     // //legend clicks
     $('.clearAll').click(function() {
         $(".fc-process, .fc-rhombus, .fc-finish").removeClass('faded');
+        draw.group();
     });
 
     $('.background').click(function() {
+        console.log('background');
         $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
         shapes.map(function(node) {
             var parent = $('#' + node.id);
@@ -24,6 +26,7 @@ $(document).ready(function() {
     });
 
     $('.studentRequired').click(function() {
+        console.log('studentRequired');
         $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
         shapes.map(function(node) {
             var parent = $('#' + node.id);
@@ -33,13 +36,112 @@ $(document).ready(function() {
                     // fade this shape
                     shape.addClass("faded");
                 } else {
-                    // make this shape peach
                     shape.removeClass("faded orange pink yellow green lightblue aqua");
                     shape.addClass("peach");
                 }
             }
         })
     });
+
+    $('.decisionRequired').click(function() {
+        console.log('decisionRequired');
+        $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
+        shapes.map(function(node) {
+            var parent = $('#' + node.id);
+            var shape = $(parent.children()[0]);
+            if (node.category) {
+                if (node.category.indexOf('decisionRequired') < 0) {
+                    // fade this shape
+                    shape.addClass("faded");
+                } else {
+                    shape.removeClass("faded peach orange pink yellow green lightblue aqua");
+                    shape.addClass("purple");
+                }
+            }
+        })
+    });
+
+    $('.communication').click(function() {
+        console.log('communication');
+        $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
+        shapes.map(function(node) {
+            var parent = $('#' + node.id);
+            var shape = $(parent.children()[0]);
+            if (node.category) {
+                if (node.category.indexOf('communication') < 0) {
+                    // fade this shape
+                    shape.addClass("faded");
+                } else {
+                    shape.removeClass("faded orange peach pink green lightblue aqua purple");
+                    shape.addClass("yellow");
+                }
+            }
+        })
+    });
+
+    $('.aioStep').click(function() {
+        console.log('aioStep');
+        $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
+        shapes.map(function(node) {
+            var parent = $('#' + node.id);
+            var shape = $(parent.children()[0]);
+            if (node.category) {
+                if (node.category.indexOf('aioStep') < 0) {
+                    // fade this shape
+                    shape.addClass("faded");
+                } else {
+                    shape.removeClass("faded orange peach pink yellow lightblue aqua purple");
+                    shape.addClass("green");
+                }
+            }
+        })
+    });
+
+    $('.hosStep').click(function() {
+        console.log('hosStep');
+        $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
+        shapes.map(function(node) {
+            var parent = $('#' + node.id);
+            var shape = $(parent.children()[0]);
+            if (node.category) {
+                if (node.category.indexOf('hosStep') < 0) {
+                    // fade this shape
+                    shape.addClass("faded");
+                } else {
+                    shape.removeClass("faded orange peach pink yellow green aqua purple");
+                    shape.addClass("lightblue");
+                }
+            }
+        })
+    });
+
+    $('.outcome').click(function() {
+        console.log('outcome');
+        $(".fc-process, .fc-rhombus, .fc-finish").addClass("faded");
+        shapes.map(function(node) {
+            var parent = $('#' + node.id);
+            var shape = $(parent.children()[0]);
+            if (node.category) {
+                if (node.category.indexOf('outcome') < 0) {
+                    // fade this shape
+                    shape.addClass("faded");
+                } else {
+                    shape.removeClass("faded orange peach pink yellow green lightblue purple");
+                    shape.addClass("aqua");
+                }
+            }
+        })
+    });
+
+
+
+
+
+
+
+
+
+
 
 
     //back to top
